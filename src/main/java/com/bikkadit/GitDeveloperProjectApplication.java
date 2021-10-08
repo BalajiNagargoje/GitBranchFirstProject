3275a02c6568e212dffdc30bbcef5e0d859a01ee
BalajiNagargoje <balajinagargoje2410@gmail.com>
2021-10-08 22:56:08 +0530

    first changes in Apply

--- src/main/java/com/bikkadit/GitDeveloperProjectApplication.java
+++ src/main/java/com/bikkadit/GitDeveloperProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDeveloperProjectApplication {
 
+	String s="Balaji";
 	public static void main(String[] args) {
 		SpringApplication.run(GitDeveloperProjectApplication.class, args);
 	}
