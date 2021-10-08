f68e5b6e78a15af3d2aae17d97b309df9f78d3f6
BalajiNagargoje <balajinagargoje2410@gmail.com>
2021-10-08 23:13:34 +0530

    IES-3: New Variable data from Vikas

--- src/main/java/com/bikkadit/GitDeveloperProjectApplication.java
+++ src/main/java/com/bikkadit/GitDeveloperProjectApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDeveloperProjectApplication {
 
+	
+	
+	String s1="Vikas";
 	public static void main(String[] args) {
 		SpringApplication.run(GitDeveloperProjectApplication.class, args);
 	}
