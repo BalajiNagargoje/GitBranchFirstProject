97815805d5397b3a9db56f9324b229bcf5174df2
BalajiNagargoje <balajinagargoje2410@gmail.com>
2021-10-08 22:36:37 +0530

    IES 1:Project created

---
+++ src/main/java/com/bikkadit/GitDeveloperProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDeveloperProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDeveloperProjectApplication.class, args);
+	}
+
+}
